828a0e76896d4df1e42d44df429966d79def62a3
Haojun Du <haojun.du@nomura.com>
2018-06-26 11:45:06 +0800

    sum3closest

--- src/javacode/leetcodeArraySet/Sum3Cloest.java
+++ src/javacode/leetcodeArraySet/Sum3Cloest.java
@@ -1,5 +1,7 @@
 package javacode.leetcodeArraySet;
 
+import java.util.Arrays;
+
 /**
  * 16. 3Sum Closest
  * Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target. 
@@ -11,13 +13,41 @@ package javacode.leetcodeArraySet;
  */
 public class Sum3Cloest {
 	
+	/**
+	 * two pointers algorithm
+	 * 
+	 * 1. sort integer array
+	 * 2. loop each integer
+	 * 3. sum = nums[i] + nums[left] + nums[right]
+	 * 4. delta = Math.min(delta, Math.abs(sum-target))
+	 * 5. if(Math.abs(sum-target) < delta) result = sum
+	 */
 	public int threeSumClosest(int[] nums, int target) {
-		return 0;
+		Arrays.sort(nums);
+		int delta= Integer.MAX_VALUE;
+		int result= Integer.MAX_VALUE;
+		for(int i=0; i<nums.length-2; i++){
+			int left = i+1,  right= nums.length-1;
+			while(left < right){
+				if(Math.abs(nums[left] + nums[right] + nums[i] -target)<delta){
+					result = nums[left] + nums[right] + nums[i];
+					delta = Math.abs(nums[left] + nums[right] + nums[i] -target);
+				}
+				if( nums[left] + nums[right] + nums[i] == target) return target;
+				if( nums[left] + nums[right] + nums[i]> target ){
+					right--;
+				} else {
+					left++;
+				}
+			}
+		}
+		return result;
     }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Sum3Cloest sum = new Sum3Cloest();
+		int[] nums = {-1, 2, 1, -4};
+		System.out.println(sum.threeSumClosest(nums, 10));
 	}
 
 }
