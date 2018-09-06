5f3d094f45b589c3a0c417a08ff57f402cd69731
hawking <haojun_du@126.com>
2018-09-06 11:02:23 +0800

    107

---
+++ src/javacode/leetcodeTreeSet/SymmetricTree.java
@@ -0,0 +1,25 @@
+package javacode.leetcodeTreeSet;
+
+import javacode.entity.TreeNode;
+
+/**
+ *  101. Symmetric Tree
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+ *
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ */
+public class SymmetricTree {
+
+    public boolean isSymmetric(TreeNode root) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
